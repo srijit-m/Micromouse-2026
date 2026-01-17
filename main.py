116ccf5a28c3cf519359ac3b947d024abb14ff60
srijit-m <srijit.mukherjee@uqconnect.edu.au>
2026-01-17 15:48:30 +1000

    Have checked encoders, the library used for encoders is incorrect as the first motor is getting double the readings of the second motor

--- main.py
+++ main.py
@@ -11,14 +11,27 @@ mm = Micromouse()
 
 
 if __name__ == "__main__":   
+    
     mm.led_green_set(1)
     mm.led_red_set(0)
-    mm.invert_motor_1()
-    mm.drive_forward(100)
-    time.sleep(5)
+    encoder_tuple  = mm.get_encoders()
+    print(encoder_tuple)
+    mm.drive_forward(125)
+    time.sleep(3)
     mm.drive_stop()
     mm.led_red_set(1)
     mm.led_green_set(0)
+    encoder_tuple = mm.get_encoders()
+    print(encoder_tuple)
+    """
+    current_time = time.time()
+    while time.time()-current_time < 10:
+        mm.led_green_set(1)
+        mm.led_red_set(0)
+        mm.drive_forward(100)
+        encoder_tuple = mm.get_encoders()
+        print(encoder_tuple)
 
+    """
     
 
