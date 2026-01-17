d4152ccb764ea890bc0bc459a548b4ee8ecc5f5f
srijit-m <srijit.mukherjee@uqconnect.edu.au>
2026-01-17 22:58:30 +1000

    This is the main file with encoder based movement

--- main.py
+++ main.py
@@ -12,17 +12,19 @@ mm = Micromouse()
 
 if __name__ == "__main__":   
     
-    mm.led_green_set(1)
-    mm.led_red_set(0)
-    encoder_tuple  = mm.get_encoders()
-    print(encoder_tuple)
-    mm.drive_forward(120)
-    time.sleep(3)
-    mm.drive_stop()
     mm.led_red_set(1)
     mm.led_green_set(0)
-    encoder_tuple = mm.get_encoders()
-    print(encoder_tuple)
+    encoder_1_reading = mm.get_encoder_1_counts()
+    print(encoder_1_reading)
+    mm.drive_forward(100)
+    while (mm.get_encoder_1_counts() < 1000):
+        print(mm.get_encoder_1_counts())
+        time.sleep_ms(10)
+
+    mm.drive_stop()
+    mm.led_red_set(0)
+    mm.led_green_set(1)
+    
     """
     current_time = time.time()
     while time.time()-current_time < 10:
